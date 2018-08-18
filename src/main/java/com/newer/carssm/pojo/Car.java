9d5dc3e868dfe39a14b31c38e58a26fbf0d01a09
chenzhengjie <398468248@qq.com>
2018-08-18 15:05:39 +0800

    实体类 Car

---
+++ src/main/java/com/newer/carssm/pojo/Car.java
@@ -0,0 +1,25 @@
+package com.newer.carssm.pojo;
+
+public class Car {
+    private String name;
+    private double price;
+
+    public Car() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
